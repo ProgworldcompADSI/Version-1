833f7b7e3c57f320ec5789ec264e627c8a4063fd
ProgworldcompADSI <jdtobar30@misena.edu.co>
2018-04-17 15:20:10 -0500

    Pantalla de Ingreso
    En este codigo se puede observar las opciones de ingreso del usuario y administrador.

---
+++ loginF.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+
+<html><head>
+
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Equipe Etoile</title>
+		
+<style>#free-flash-header a,#free-flash-header a:hover {color:#363636;}#free-flash-header a:hover {text-decoration:none}</style>
+<link rel="shortcut icon" type="image/ico" href="imagenes/icono.png"  />
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+		<link href="bootstrap/css/style.css" rel="stylesheet" type="text/css" media="all">
+<style type="text/css">
+	.cuatro a {
+
+		
+  border-radius: 50px 50px 50px 50px / 50px 50px 50px 50px;
+     
+		
+	}
+	.cuatro a:hover {
+		
+border-radius: 50px 50px 50px 50px / 50px 50px 50px 50px;
+		
+		background-color: #5F5D5D;
+		padding: 1px;
+		border-top-width: 14px;
+		border-top-style: ridge;
+		border-top-color: #ffbf00;
+		border-right-color: #000;
+		border-bottom-color: #ffbf00;
+		border-left-color: #000;
+		border-right-width: 14px;
+		border-bottom-width: 14px;
+		border-left-width: 14px;
+		border-right-style: ridge;
+		border-bottom-style: ridge;
+		border-left-style: ridge;
+	}
+
+	</style>
+		
+
+			
+</head>
+<body>
+
+	<div class="page-container">
+    		
+		<header>
+			<div class="container dark-bg no_left no_right">
+            <div class="col-md-4 col-xs-12 no_left">
+						
+							<img src="imagenes/LOGO-FINAL2.png" width="250" height="150">
+					
+					</div>
+				<div class="row">
+
+		
+					<?php include_once'../modelo/header.php';?>
+
+				</div>
+			</div>
+		</header>
+<br><br>
+<center><font color="#ffbf00" size="+3" face="Trebuchet MS, Arial, Helvetica, sans-serif">INICIAR SESIÓN</font></center><br><br><br><br>
+
+
+		<div class="container">
+        <div class="row">
+            <div class="box">
+             
+                <div class="col-sm-12">
+                <center>
+            <div class="cuatro">
+            <table><tr><td>
+            <a href="loginA.php"><br>
+            <center>
+            <font color="#ffbf00" size="+2" face="Trebuchet MS, Arial, Helvetica, sans-serif"><B>ADMINISTRADOR</B></font><br><br>
+            </center>
+            <img src="imagenes/admin.png" width="200" height="200" ></a><br><br><br>
+            <td width="200"></td></td></td>
+            
+            <td>
+            <a href="login.php"><br>
+            <center>
+            <font color="#ffbf00" size="+2" face="Trebuchet MS, Arial, Helvetica, sans-serif"><B>USUARIO </B></font><br><br>
+            </center>
+            <img src="imagenes/user2.png" width="200" height="200"></a><br><br><br>
+            </td></tr></table><br></div>
+
+            </center>
+        
+    </div> 
+
+</div>
+					</div>
+
+
+
+		</footer>
+	
+	</div>
+  </body>
+</html>
