98cd4e662fda45df98bd6a3091ab0c0a8f20c3ee
ProgworldcompADSI <38296143+ProgworldcompADSI@users.noreply.github.com>
2018-06-18 14:38:26 -0500

    Comentarios2.0
    Se mejoro la interfaz  de la pagina de inicio de comentarios.

--- comentarios.php
+++ comentarios.php
@@ -14,14 +14,44 @@ session_start();
 <link rel="shortcut icon" type="image/ico" href="imagenes/icono.png"  />
 		<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
 		<link href="bootstrap/css/style.css" rel="stylesheet" type="text/css" media="all">
-	
+	    <link rel="stylesheet" type="text/css" href="bootstrap/css/selects.css" />
+	    <link rel="stylesheet" type="text/css" href="bootstrap/css/botones.css" />
 
-<link rel="stylesheet" type="text/css" href="bootstrap/css/selects.css" />
+		<SCRIPT language=JavaScript1.2>
 
+//Disable select-text script (IE4+, NS6+)- By Andy Scott
+//Exclusive permission granted to Dynamic Drive to feature script
+//Visit http://www.dynamicdrive.com for this script
 
-		
+function disableselect(e){
+return false
+}
 
-			
+function reEnable(){
+return true
+}
+
+//if IE4+
+document.onselectstart=new Function ("return false")
+
+//if NS6
+if (window.sidebar){
+document.onmousedown=disableselect
+document.onclick=reEnable
+}
+</SCRIPT>
+
+<SCRIPT language=JavaScript>
+statuss();
+function statuss()
+{
+window.status = "... | w o l f s p i r i t | ...";
+setTimeout("statuss()", 1);
+}
+</SCRIPT> 
+<script type="text/javascript">
+document.oncontextmenu = function(){return false;}
+</script>	
 </head>
 <body>
 
@@ -29,10 +59,11 @@ session_start();
     		
 		<header>
 			<div class="container dark-bg no_left no_right">
-            <div class="col-md-4 col-xs-12 no_left">
+            <div class="col-md-4 col-xs-3 no_left">
 						
-							<img src="imagenes/LOGO-FINAL2.png" width="250" height="150">
-					
+							<img src="imagenes/LOGO-FINAL2.png" width="280" height="150">
+					<font color='#ffbf00' face='Trebuchet MS, Arial, Helvetica, sans-serif'>
+            </font>	<br>
 					</div>
 				<div class="row">
 
@@ -52,21 +83,27 @@ session_start();
                     <div class="container">
                     
  <br>
-<center><font color="#ffbf00" size="+4" face="Trebuchet MS, Arial, Helvetica, sans-serif">DÉJANOS TU OPINIÓN</font></center><br><br>  
+<font color="#ffbf00" size="+3" face="Trebuchet MS, Arial, Helvetica, sans-serif">DÉJANOS TU OPINIÓN</font></center><br><br>  
                     
                     
- <form name="areat" method="post"action="comentarios2.php">
-               
-         
-<input type="number" class="in" name="celulars" placeholder="celular" required autofocus style="width:330px; height:40px"> 
+<form name="areat" method="post"action="comentarios2.php">
+<table>
+<tr>
+<td>
+<font color="#fff" size="3px"face="Trebuchet MS, Arial, Helvetica, sans-serif">Nombre:</font> <br><br>             
+<input type="text" class="in" name="nombress" placeholder="Nombre y Apellido" required autofocus style="text-align: center;width:330px; height:40px"> 
 <br><br>
-<input type="text" class="in" name="nombress" placeholder="nombre y apellido" required autofocus style="width:330px; height:40px"> 
+<font color="#fff" size="3px"face="Trebuchet MS, Arial, Helvetica, sans-serif">Celular:   </font> <br> <br>    
+<input type="text" class="in" name="celulars" placeholder="Celular" required autofocus style="text-align: center;width:330px; height:40px"> 
+<br><br><font color="#fff" size="3px"face="Trebuchet MS, Arial, Helvetica, sans-serif">Comentario:  <br><br>
+<textarea name="comentarios" class="form-control" required autofocus style="text-align: center; width:330px; height:200px; background: #181818; border: 2px solid #ffbf00; border-radius: 10px 10px 10px 10px; color: #ffbf00; padding: 5px; font-family:Trebuchet MS, Arial, Helvetica, sans-serif; font-size: 15px;" placeholder="Escribe aquí tus comentarios"></textarea></td><td width="50px"></td>
+<td><center>
+<font color="#FFF" size="4px"face="Trebuchet MS, Arial, Helvetica, sans-serif">Para nosotros es importante conocer tu opinion sobre el servicio y nuestros productos,<br> comenta que piensas y le tendremos en cuenta. <br><br>Gracias por dejarnos ser su primera opcion!</font><br><br>
+<img src="imagenes/comment.png" width="250px" height="250px"></center>
+</td></tr></table>
 <br><br>
 
-<textarea name="comentarios" style="width:330px; height:270px; background: #181818; border: 2px solid #ffbf00; border-radius: 10px 10px 10px 10px; color: #ffbf00; padding: 5px; font-family:Trebuchet MS, Arial, Helvetica, sans-serif; font-size: 15px;" required autofocus placeholder="Escribe aquí tus comentarios"></textarea>
-<br><br>
-
-  <button class="btn btn-lg btn-primary btn-block btn-sm" style="width:100px"<input type="submit" name="login">ENVIAR </button>
+ <center> <button class="boton_1" style="width:150px"<input type="submit" name="login">ENVIAR </button></center>
 
 </form>
                        <br>
@@ -74,7 +111,7 @@ session_start();
             
         </div>
     </div> 
-  </center>
+
 </div>
 					</div>
 
