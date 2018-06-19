8bf5e704010f7f2255df4d505cbc971c3c0e267e
ProgworldcompADSI <38296143+ProgworldcompADSI@users.noreply.github.com>
2018-06-19 15:29:22 -0500

    Pantalla reservas historial administrador2.0
    Se mejoro la interfaz del historial de administrador

--- reservadoa3.php
+++ reservadoa3.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <?php
 session_start();
-?>
+if ($_SESSION['estado']!="1")
+{
+	header("Location: logout.php");
+}
+	?>
 <html><head>
 
 		<meta charset="utf-8">
@@ -14,55 +18,10 @@ session_start();
 		<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
 		<link href="bootstrap/css/style.css" rel="stylesheet" type="text/css" media="all">
         <link rel="stylesheet" href="bootstrap/css/style2.css" type="text/css" media="screen"/>
-	
-<style type="text/css">
-
-* {
- padding:0px;
- margin:0px;
- font-family: Trebuchet MS, Arial, Helvetica, sans-serif;
-
-}
-
-
-ul, ol {
- list-style:none;
-}
-
-.nav li a {
-background-color:#333;
- color:#ffbf00;
- text-decoration:none;
- padding: 10px 15px;
- display:block;
-}
-
-.nav li a:hover {
- background-color:#666;
-}
-
-
-.nav li ul {
- display:none;
- position:absolute;
-}
-
-.nav li:hover > ul {
- display:block;
-}
-
-.nav li ul li {
- position:relative;
-}
-
-.nav li ul li ul {
- right:-140px;
- top:0px;
-}
-
-</style>
-		
-
+           	    <link rel="stylesheet" type="text/css" href="bootstrap/css/infoV.css" />
+                	    <link rel="stylesheet" type="text/css" href="bootstrap/css/botones.css" />
+                        <link rel="stylesheet" type="text/css" href="bootstrap/css/cuatro.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/selects.css" />
 			
 </head>
 <body>
@@ -71,7 +30,7 @@ background-color:#333;
     		
 		<header>
 			<div class="container dark-bg no_left no_right">
-            <div class="col-md-4 col-xs-12 no_left">
+             <div class="col-md-4 col-xs-3 no_left">
 						
 							<img src="imagenes/LOGO-FINAL2.png" width="280" height="150">
 					
@@ -90,83 +49,98 @@ background-color:#333;
         <div class="row">
             <div class="box">
                 <div class="col-lg-12">
-                  <center>
- <table>
- <th width="163">
+ <center>
+ <br><br>
 
-<div id="">
- <ul class="nav">
      
-        <li><a href="reservadoa.php"><center>OPCIONES DE RESERVA</center></a>
-         <ul>
-          <li><a href="reservadoa1.php">RESERVAS DEL DIA DE HOY</a></li>
-             <li><a href="reservadoa2.php">CONSULTA RESERVA POR CEDULA</a></li>
-               <li><a href="reservadoa4.php"><font size="1px">CONSULTAR RESERVA POR EMPLEADO</font></a> 
-               <ul><a href="reservadoa5.php"><font size="1px">RESERVAS POR EMPLEADO DE HOY</a></font></ul></li>
-             <li><a href="reservadoa3.php">HISTORIAL DE RESERVAS</a> </li>
+      <select id="status" name="status" onChange="location.href=this.value" size="1" class="sel">
+       
+        <option value="reservadoa3.php">RESERVAS POR EMPLEADO</option>
+        <option value="reservadoa.php">HISTORIA DE RESERVAS</option>
+        <option value="reservadoa1.php">RESERVAS POR FECHA</option>
+         <option value="reservadoa2.php">RESERVAS POR CEDULA</option>
+     
+		</select>
 
-            </ul>
-        </li>
-        
- </ul>
-</div>
-</th>
-</table>
+
+<br><br><br><br>       
+
+ <center><font color="#FF3300" size="+3" face="Trebuchet MS, Arial, Helvetica, sans-serif">RESERVAS:</font></center><br><br>
+
+
+
+ <form name="areat" method="post"action="reservadoa3.php">
+
+ EMPLEADO A CONSULTAR:     
+<input type="text" class="form-control" name="empleadoa" placeholder="Empleado" required autofocus style="width:350px; height:40px"> 
+<br>
+
+  <button class="boton_1" style="width:200px"<input type="submit" name="login">INGRESAR </button>
 <br><br>
-                    
-
-  <center><font color="#FF3300" size="+3" face="Trebuchet MS, Arial, Helvetica, sans-serif">RESERVAS:</font></center><br><br>
-
+</form>
  
  <?php
+ 
  error_reporting(E_ERROR);
  $mysql=new mysqli("localhost","root","","peluqueria");
  if ($mysql->connect_error)
- die("Problemas con la conexión a la base de datos");
+ die("Problemas con la conexiأ╠n a la base de datos");
  $d1=($_SESSION['usuario']);
  $a2=($_SESSION['clave']);
+ $a5=$_POST['empleadoa'];
+
  
- 
- $registro=$mysql->query("SELECT usuario.nombre,usuario.apellido,usuario.documento,idreservas, fecha, hora, servicio,precio,usuario, empleado,estado FROM reservas INNER JOIN usuario ON reservas.usuario=usuario.documento") or
+ $registro=$mysql->query("SELECT usuario.nombre,usuario.apellido,usuario.documento,idreservas, fecha, hora, servicio,precio,usuario, empleado,estado FROM reservas INNER JOIN usuario ON reservas.usuario=usuario.documento where reservas.empleado like'%$a5%' ") or
  die($mysql->error);
 
    echo '<table class="tabla1">';
- echo '<tr ><thead><th><center> CLIENTE  </th><th>   IDENTIFICACION </th><th><center>  FECHA  </th><th>  HORA </th> <th>SERVICIO </th><th>PRECIO </th><th>EMPLEADO </th><th>ESTADO </th></tr></thead>';
- while ($reg=$registro->fetch_array())
+   //
+ echo '<tr ><thead><th><center> EMPLEADO  </th><th>   CLIENTE </th><th><center>  SERVICIO  </th><th>  FECHA </th> <th>HORA </th><th>PRECIO </th><th>ESTADO </th><th> </th><th> </th></tr></thead>';
+
+while ($reg=$registro->fetch_array())
  {
  echo '<tr><tbody>';
- echo '<td WIDTH="800" 
+ echo '<td WIDTH="600" 
 	    HEIGHT="35">';
+ echo $reg['empleado'];
+ echo '</td>';
+ 
+ echo '<td WIDTH="600">';
  echo $reg['nombre'].' '.$reg['apellido'];
  echo '</td>';
- echo '<td WIDTH="80">';
- echo $reg['documento'];
- echo '</td>';
- echo '<td WIDTH="800">';
- echo $reg['fecha'];
- echo '</td>';
   echo '<td WIDTH="150">';
- echo $reg['hora'];
+ echo $reg['servicio'];
  echo '</td>';
    echo '<td WIDTH="200">';
- echo $reg['servicio'];
+ echo $reg['fecha'];
+ echo '</td>';
+  echo '<td WIDTH="200">';
+ echo $reg['hora'];
  echo '</td>';
   echo '<td WIDTH="200">';
  echo $reg['precio'];
  echo '</td>';
   echo '<td WIDTH="200">';
- echo $reg['empleado'];
- echo '</td>';
-  echo '<td WIDTH="200">';
  echo $reg['estado'];
+  echo '<td WIDTH="500">';
+echo "<font color='#CC0000'><a href='cancelara.php?idreservas=$reg[idreservas] '>CANCELAR <img src='imagenes/ex.png' width='30' height='30'></a>";
+echo '<td WIDTH="500">';
+echo "<font color='#CC0000'><a href='realizara.php?idreservas=$reg[idreservas] '>REALIZADO <img src='imagenes/chulo.png' width='30' height='30'></a>";
  echo '</td></tbody>';
-
  
- }
 
+
+?>
+<form action="reportes_empleado.php" method="post" target="_blank">
+ <input type="hidden" class="in" style="text-align: center; width:225px; height:30px" name="emp" size="50" value="<?php echo $a5;?>"  > 
+<?php
+  }
  $mysql->close();
  ?>
-
+  </table>
+<br><br><br>
+<button class="boton_2" style="width:250px" input type="submit" name="login">GENERAR REPORTE </button>
+	</form>
  </center>
  
 
