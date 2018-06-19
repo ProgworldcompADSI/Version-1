c7e84cb37c9745fcbdad4b97ba077c6c6f6b1a98
ProgworldcompADSI <38296143+ProgworldcompADSI@users.noreply.github.com>
2018-06-19 15:26:19 -0500

    Pantallla reservas administrador 2.0
    Se mejoro la interfaz  del administrador

--- reservadoa1.php
+++ reservadoa1.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <?php
 session_start();
-?>
+if ($_SESSION['estado']!="1")
+{
+	header("Location: logout.php");
+}
+	?>
 <html><head>
 
 		<meta charset="utf-8">
@@ -14,54 +18,11 @@ session_start();
 		<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
 		<link href="bootstrap/css/style.css" rel="stylesheet" type="text/css" media="all">
         <link rel="stylesheet" href="bootstrap/css/style2.css" type="text/css" media="screen"/>
+           	    <link rel="stylesheet" type="text/css" href="bootstrap/css/infoV.css" />
+                	    <link rel="stylesheet" type="text/css" href="bootstrap/css/botones.css" />
+                        <link rel="stylesheet" type="text/css" href="bootstrap/css/cuatro.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/selects.css" />
 	
-<style type="text/css">
-
-* {
- padding:0px;
- margin:0px;
- font-family: Trebuchet MS, Arial, Helvetica, sans-serif;
-
-}
-
-
-ul, ol {
- list-style:none;
-}
-
-.nav li a {
-background-color:#333;
- color:#ffbf00;
- text-decoration:none;
- padding: 10px 15px;
- display:block;
-}
-
-.nav li a:hover {
- background-color:#666;
-}
-
-
-.nav li ul {
- display:none;
- position:absolute;
-}
-
-.nav li:hover > ul {
- display:block;
-}
-
-.nav li ul li {
- position:relative;
-}
-
-.nav li ul li ul {
- right:-140px;
- top:0px;
-}
-
-</style>
-		
 
 			
 </head>
@@ -71,14 +32,13 @@ background-color:#333;
     		
 		<header>
 			<div class="container dark-bg no_left no_right">
-            <div class="col-md-4 col-xs-12 no_left">
+            <div class="col-md-4 col-xs-3 no_left">
 						
 							<img src="imagenes/LOGO-FINAL2.png" width="280" height="150">
 					
 					</div>
 				<div class="row">
-
-		
+				
 					<?php include_once'../modelo/header3.php';?>
 
 				</div>
@@ -91,32 +51,41 @@ background-color:#333;
             <div class="box">
                 <div class="col-lg-12">
                   <center>
- <table>
- <th width="163">
-
-<div id="">
- <ul class="nav">
-     
-        <li><a href="reservadoa.php"><center>OPCIONES DE RESERVA</center></a>
-         <ul>
-          <li><a href="reservadoa1.php">RESERVAS DEL DIA DE HOY</a></li>
-             <li><a href="reservadoa2.php">CONSULTA RESERVA POR CEDULA</a></li>
-               <li><a href="reservadoa4.php"><font size="1px">CONSULTAR RESERVA POR EMPLEADO</font></a> 
-               <ul><a href="reservadoa5.php"><font size="1px">RESERVAS POR EMPLEADO DE HOY</a></font></ul></li>
-             <li><a href="reservadoa3.php">HISTORIAL DE RESERVAS</a> </li>
-
-            </ul>
-        </li>
-        
- </ul>
-</div>
-</th>
-</table>
 <br><br>
+
+     
+      <select id="status" name="status" onChange="location.href=this.value" size="1" class="sel">
+       
+        <option value="reservadoa1.php">RESERVAS POR FECHA</option>
+        <option value="reservadoa2.php">RESERVAS POR CEDULA</option>
+        <option value="reservadoa3.php">RESERVAS POR EMPLEADO</option>
+        <option value="reservadoa.php">HISTORIA DE RESERVAS</option>
+     
+		</select>
+
+
+<br><br><br><br>
                     
 
-  <center><font color="#FF3300" size="+3" face="Trebuchet MS, Arial, Helvetica, sans-serif">RESERVAS:</font></center><br><br>
-
+<center><font color="#FF3300" size="+3" face="Trebuchet MS, Arial, Helvetica, sans-serif">CONSULTA DE RESERVAS:</font></center><br><br>
+ <form name="areat" method="post"action="../controlador/libreria.php">
+ <font color="#fffffff" size="+1">FECHA: <br><br><br>DE:
+<input type="date" class="in" style="text-align: center; width:225px; height:30px" name="de" size="50" value="<?php if(isset($_REQUEST['dato1'])) 
+{ echo $_REQUEST['dato1']; }?>"  >
+   HASTA: <input type="date" class="in" style="text-align: center; width:225px; height:30px" name="hasta" size="50" value="<?php if(isset($_REQUEST['dato2'])) 
+{ echo $_REQUEST['dato2']; }?>"  ><br><br>
+ 
+<br><br>
+<font color="#ffbf00" size="-1"><b>ESCOJA EL ESTADO A CONSULTAR<font color="#FF3300" size="+1"> *</font> : </b></font>
+<font color="#ffffff" size="-1">
+ <input type="checkbox" name="activo" value="ACTIVO" > ACTIVOS
+ <input type="checkbox" name="realizados" value="REALIZADO" > REALIZADOS
+  <input type="checkbox" name="cancelados" value="CANCELADO"> CANCELADOS
+<br><br><br>
+  <button class="boton_1" style="width:200px"<input type="submit" name="login">CONSULTAR </button>
+<br><br><br>
+</form>
+ 
  
  <?php
  error_reporting(E_ERROR);
@@ -125,13 +94,14 @@ background-color:#333;
  die("Problemas con la conexión a la base de datos");
  $d1=($_SESSION['usuario']);
  $a2=($_SESSION['clave']);
- 
- 
- $registro=$mysql->query("SELECT usuario.nombre,usuario.apellido,usuario.documento,idreservas, fecha, hora, servicio,precio,usuario, empleado,estado FROM reservas INNER JOIN usuario ON reservas.usuario=usuario.documento where fecha =CURRENT_DATE() and  reservas.estado='activo'") or
+
+
+ $registro=$mysql->query("SELECT usuario.nombre,usuario.apellido,usuario.documento,idreservas, fecha, hora, servicio,precio,usuario, empleado,estado FROM reservas INNER JOIN usuario ON reservas.usuario=usuario.documento WHERE   reservas.fecha BETWEEN '$_REQUEST[dato1]' and '$_REQUEST[dato2]' and (reservas.estado='$_REQUEST[dato6]'or reservas.estado='$_REQUEST[dato7]' or reservas.estado='$_REQUEST[dato8]') ") or
  die($mysql->error);
 
+ echo "<center>";
    echo '<table class="tabla1">';
- echo '<tr ><thead><th>CLIENTE  </th><th>   IDENTIFICACION </th><th>  FECHA  </th><th>  HORA </th> <th>SERVICIO </th><th>PRECIO </th><th>EMPLEADO </th><th>ESTADO </th><th>   </th><th>    </th></tr></thead>';
+ echo '<tr ><thead><th>CLIENTE  </th><th>   IDENTIFICACION </th><th>  FECHA  </th><th>  HORA </th> <th>SERVICIO </th><th>PRECIO </th><th>EMPLEADO </th><th>ESTADO </th><th> </th><th> </th></tr></thead>';
  while ($reg=$registro->fetch_array())
  {
  echo '<tr><tbody>';
@@ -139,7 +109,7 @@ background-color:#333;
 	    HEIGHT="35">';
  echo $reg['nombre'].' '.$reg['apellido'];
  echo '</td>';
- echo '<td WIDTH="120">';
+ echo '<td WIDTH="100">';
  echo $reg['documento'];
  echo '</td>';
  echo '<td WIDTH="800">';
@@ -159,17 +129,40 @@ background-color:#333;
  echo '</td>';
   echo '<td WIDTH="200">';
  echo $reg['estado'];
-  echo '<td WIDTH="500">';
-echo "<font color='#CC0000'><a href='reservadoa1.php?idreservas=$reg[idreservas] '>CANCELAR <img src='imagenes/ex.png' width='30' height='30'></a>";
  echo '<td WIDTH="500">';
-echo "<font color='#CC0000'><a href='reservadoa1.php?idreservas=$reg[idreservas] '>REALIZADO <img src='imagenes/chulo.png' width='30' height='30'></a>";
+echo "<font color='#CC0000'><a href='cancelara.php?idreservas=$reg[idreservas] '>CANCELAR <img src='imagenes/ex.png' width='30' height='30'></a>";
+echo '<td WIDTH="500">';
+echo "<font color='#CC0000'><a href='realizara.php?idreservas=$reg[idreservas] '>REALIZADO <img src='imagenes/chulo.png' width='30' height='30'></a>";
+
  echo '</td></tbody>';
+ ?>
 
-  }
+<form action="reportes_fecha.php" method="post" target="_blank"> 
+ 
+ <input type="hidden" class="in" style="text-align: center; width:225px; height:30px" name="dee" size="50" value="<?php if(isset($_REQUEST['dato1'])) 
+{ echo $_REQUEST['dato1']; }?>"  >
+ <input type="hidden" class="in" style="text-align: center; width:225px; height:30px" name="hastaa" size="50" value="<?php if(isset($_REQUEST['dato2'])) 
+{ echo $_REQUEST['dato2']; }?>"  >
+ <input type="hidden" class="in" style="text-align: center; width:225px; height:30px" name="act" size="50" value="<?php if(isset($_REQUEST['dato6'])) 
+{ echo $_REQUEST['dato6']; }?>"  >
+ <input type="hidden" class="in" style="text-align: center; width:225px; height:30px" name="rea" size="50" value="<?php if(isset($_REQUEST['dato7'])) 
+{ echo$_REQUEST['dato7']; }?>"  >
+ <input type="hidden" class="in" style="text-align: center; width:225px; height:30px" name="canc" size="50" value="<?php if(isset($_REQUEST['dato8'])) 
+{ echo $_REQUEST['dato8']; }?>"  >
 
-
+ <?php
+ 
+}
  $mysql->close();
  ?>
+ </table>
+<br><br><br>
+<button class="boton_2" style="width:250px" input type="submit" name="login">GENERAR REPORTE </button>
+	</form>
+
+<br><br>
+
+ </center>
 
  </center>
  
@@ -178,5 +171,3 @@ echo "<font color='#CC0000'><a href='reservadoa1.php?idreservas=$reg[idreservas]
 </html>
  
  
-
-
