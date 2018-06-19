fea71b2f7227bfbbcef3e2c389b1d72c7a9f5da1
ProgworldcompADSI <38296143+ProgworldcompADSI@users.noreply.github.com>
2018-06-19 15:19:15 -0500

    Pantalla de Registro2.0
    Se cambio el formulario de registro mejorando la interfaz para el usuario.

--- registro.php
+++ registro.php
@@ -7,13 +7,14 @@
 		<title>Equipe Etoile</title>
 		
 <style>#free-flash-header a,#free-flash-header a:hover {color:#363636;}#free-flash-header a:hover {text-decoration:none}</style>
+
+
+
 <link rel="shortcut icon" type="image/ico" href="imagenes/icono.png"  />
 		<link href="bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
 		<link href="bootstrap/css/style.css" rel="stylesheet" type="text/css" media="all">
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/selects.css" />
-		<link rel="stylesheet" type="text/css" href="bootstrap/css/botones.css" />
-	
-
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/botones.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/selects.css" />
 
 	
 </head>
@@ -24,55 +25,34 @@
     		
 		<header>
 			<div class="container dark-bg no_left no_right">
-            <div class="col-md-4 col-xs-12 no_left">
+            <div class="col-md-4 col-xs-3 no_left">
 						
-							<img src="imagenes/LOGO-FINAL2.png" width="250" height="150">
+							<img src="imagenes/LOGO-FINAL2.png" width="280" height="150">
 					
 					</div>
 				<div class="row">
 
-		
+	
 					<?php include_once'../modelo/header.php';?>
 
 				</div>
 			</div>
 		</header>
-<br><br>
-<center><font color="#ffbf00" size="+3" face="Trebuchet MS, Arial, Helvetica, sans-serif">REGISTRARSE</font></center><br><br>
-<div class="row">
-				<div class="col-md-12 col-xs-12">
-					
-				</div>
-               <center> <font face="Trebuchet MS, Arial, Helvetica, sans-serif" color="#FFF"><p><h4>Complete todos los campos requeridos para su registro en nuestra base de datos.</p></center><br><br></h4></font>
-              
-			</div>
-		<div><div class="container">
+
+	<div class="container">
 
         <div class="row">
-            <div class="box">
+            
                 <div class="col-lg-12">
-                  <center>
-                    <div class="container">
-        <div class="card card-container">
-                      
-<form class="form-signin" method="post"action="registro2.php">
-      <span id="reauth-email" class="reauth-email"></span>
+                  
+<br><br>
+<center><font color="#ffbf00" size="+2" face="Trebuchet MS, Arial, Helvetica, sans-serif">REGISTRESE EN NUESTRA PAGINA.</font></center><br><br>
 
-<input type="text"  class="in" name="nombre" placeholder="nombre" required autofocus style="width:350px; height:40px" > 
- <br> <br>
-<input type="text"  class="in" name="apellido" placeholder="apellido" required autofocus style="width:350px; height:40px"> 
- <br>     <br> 
-       
- <input type="number"  class="in" name="documento" placeholder="documento" required autofocus style="width:350px; height:40px"> 
- <br>  <br>
+<?php 
+include 'ventana.php';
+
+?>
  
-
-<input type="number"  class="in" name="celular" placeholder="celular" required autofocus style="width:350px; height:40px">
-<br>  <br>
-<input type="password"  class="in" name="contrasena" placeholder="contrasena" required autofocus style="width:350px; height:40px"> 
-<br>      <br>          
-  <button class="boton_1" style="width:150px"<input value="CALCULAR" type="submit"name="registro">REGISTRARSE </button>
-            </form>
             <br>
             
             
@@ -81,10 +61,8 @@
   </center>
 </div>
 					</div>
-
-
-
-		</footer>
+							
+</footer>
 	
 	</div>
   </body>
